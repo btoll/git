7a9c3c2ac000246fd2740ed2856fff0ef0fe9dac
Benjamin Toll <benjam72@yahoo.com>
2015-03-09 18:12:25 -0500

    Simplified install of git tools

--- install.sh
+++ install.sh
@@ -1,59 +1,27 @@
 #!/bin/bash
+# Installation script for custom Git extensions and aliases.
 
-usage() {
-    echo "Installation script for custom Git extensions and aliases."
-    echo
-    echo "Usage: $0 [args]"
-    echo
-    echo "Args:"
-    echo "--prefix  The location where the git remote repository will be cloned."
-    echo "          Defaults to CWD."
-}
-
-while [ "$#" -gt 0 ]; do
-    OPT="$1"
-    case $OPT in
-        --help|-help|-h) usage; exit 0 ;;
-        --prefix) shift; GIT=$(echo $1 | tr -d '\r') ;;
-    esac
-    shift
-done
-
-# https://github.com/btoll/git
-if [ -n "$GIT" ]; then
-    pushd "$GIT" > /dev/null
-fi
-
-echo "[Install] Cloning remote 'git' repository into $GIT."
-echo
-git clone git@github.com:btoll/git.git
-
-echo
 echo "[Install] Creating global git aliases..."
 git config --global alias.get-hash '!sh -c '"'git show \$0 | grep commit | cut -c8-'"
 git config --global alias.goto '!sh -c '"'git show \$0 | grep commit | cut -c8- | xargs git checkout'"
 
 echo "[Install] Creating symbolic links for git extensions..."
 echo
-cd git/bin
+pushd git/bin
 ln -s "$PWD"/git-cleanup /usr/local/bin/git-cleanup
 ln -s "$PWD"/git-hub /usr/local/bin/git-hub
 ln -s "$PWD"/git-introduced /usr/local/bin/git-introduced
 ln -s "$PWD"/git-ls /usr/local/bin/git-ls
-cd -
+popd
 
 echo "[Install] Installing git man pages..."
 echo
-cd git/man
+pushd git/man
 ln -s "$PWD"/git-cleanup.1 /usr/local/share/man/man1/git-cleanup.1
 ln -s "$PWD"/git-hub.1 /usr/local/share/man/man1/git-hub.1
 ln -s "$PWD"/git-introduced.1 /usr/local/share/man/man1/git-introduced.1
 ln -s "$PWD"/git-ls.1 /usr/local/share/man/man1/git-ls.1
-cd -
-
-if [ -n "$GIT" ]; then
-    popd > /dev/null
-fi
+popd
 
 echo
 echo "[Install] Installation complete."
