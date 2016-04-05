c39117b033fc8df0d12460e8986e7a869194061a
Benjamin Toll <benjam72@yahoo.com>
2016-04-05 10:22:38 -0400

    Improve eslint pre-commit hook

--- hooks/pre-commit.d/eslint.sh
+++ hooks/pre-commit.d/eslint.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 EXIT_CODE=0
-FILES=$(git diff-index --cached --name-only HEAD 2> /dev/null)
+FILES=$(git diff-index --cached --name-only HEAD 2> /dev/null | ag ".js\b")
 
 echo "$(tput setab 7)$(tput setaf 4)[INFO]$(tput sgr0) Running $(tput bold)ESLint$(tput sgr0) pre-commit hook..."
 
 for F in $FILES; do
-    if [[ "$F" == *.js ]]; then
-        eslint "$F"
+    eslint "$F"
 
-        if [ "$?" == 1 ]; then
-            # Note that eslint's error messages are verbose enough that we don't need to have our own.
-            EXIT_CODE=1
-        fi
+    if [ "$?" == 1 ]; then
+        # Note that eslint's error messages are verbose enough that we don't need to have our own.
+        EXIT_CODE=1
     fi
 done
 
