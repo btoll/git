621ba4092a6e2317748c5f87228add0b02e3b34f
Benjamin Toll <benjam72@yahoo.com>
2016-04-12 19:47:02 -0400

    Updated eslint.sh

--- hooks/pre-commit.d/eslint.sh
+++ hooks/pre-commit.d/eslint.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
 EXIT_CODE=0
-FILES=$(git diff-index --cached --name-only HEAD 2> /dev/null | ag ".js\b")
+FILES=$(git diff-index --cached --name-only HEAD 2> /dev/null | grep ".js\b")
 
-echo "$(tput setab 7)$(tput setaf 4)[INFO]$(tput sgr0) Running $(tput bold)ESLint$(tput sgr0) pre-commit hook..."
+if [ -z "$FILES" ]; then
+    echo "$(tput setab 7)$(tput setaf 4)[INFO]$(tput sgr0) There is nothing to lint!"
+else
+    # Find the local version and use rather than the global one.
+#    ESLINT=$(find $(pwd) -name eslint.js | grep bin)
 
-for F in $FILES; do
-    eslint "$F"
+    echo "$(tput setab 7)$(tput setaf 4)[INFO]$(tput sgr0) Running $(tput bold)ESLint$(tput sgr0) pre-commit hook..."
 
-    if [ "$?" == 1 ]; then
-        # Note that eslint's error messages are verbose enough that we don't need to have our own.
-        EXIT_CODE=1
+    for F in $FILES; do
+#        "$ESLINT" "$F"
+        eslint "$F"
+
+        if [ "$?" == 1 ]; then
+            # Note that eslint's error messages are verbose enough that we don't need to have our own.
+            EXIT_CODE=1
+        fi
+    done
+
+    if [ "$EXIT_CODE" -eq 0 ]; then
+        echo "$(tput setab 7)$(tput setaf 2)[INFO]$(tput sgr0) Completed successfully."
     fi
-done
-
-if [ "$EXIT_CODE" -eq 0 ]; then
-    echo "$(tput setab 7)$(tput setaf 2)[INFO]$(tput sgr0) Completed successfully."
 fi
 
 exit $EXIT_CODE
