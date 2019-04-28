6c3a02a13c91222c46847ed840553c2c7fcadaf7
Benjamin Toll <benjam72@yahoo.com>
2019-04-28 14:58:28 -0400

    Added `git-bootstrap` man page

--- install_manpages.sh
+++ install_manpages.sh
@@ -9,10 +9,11 @@ fi
 echo "$(tput setaf 2)[INFO]$(tput sgr0) Installing git man pages..."
 echo
 pushd man
-ln -s "$PWD"/git-cleanup.1 /usr/local/share/man/man1/git-cleanup.1
-ln -s "$PWD"/git-hub.1 /usr/local/share/man/man1/git-hub.1
-ln -s "$PWD"/git-introduced.1 /usr/local/share/man/man1/git-introduced.1
-ln -s "$PWD"/git-ls.1 /usr/local/share/man/man1/git-ls.1
+ln -s "$PWD"/git-bootstrap.1 /usr/share/man/man1/git-bootstrap.1
+ln -s "$PWD"/git-cleanup.1 /usr/share/man/man1/git-cleanup.1
+ln -s "$PWD"/git-hub.1 /usr/share/man/man1/git-hub.1
+ln -s "$PWD"/git-introduced.1 /usr/share/man/man1/git-introduced.1
+ln -s "$PWD"/git-ls.1 /usr/share/man/man1/git-ls.1
 popd
 
 echo
